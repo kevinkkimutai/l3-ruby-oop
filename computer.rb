2c27bdbafe5346a42c34d18ad8c172d28fafd331
kevinkkimutai <kiruikevinkim@gmail.com>
2023-02-15 11:02:45 +0300

    updated

--- computer.rb
+++ computer.rb
@@ -1,19 +1,46 @@
 # super class
 class Computer
-    attr_Accessor :model, :country, :year
-    
-    def initialize(model, country, year)
-        @model = model
-        @country = country
-        @year = year
-    end
-    
-    def print_computer
-        puts "Computer: #{self.model}, Year: #{self.year}"
-    end
+  attr_accessor :model, :country, :year
 
+  def initialize(model, country, year)
+    @model = model
+    @country = country
+    @year = year
+  end
+
+  def print_computer
+    puts "Computer: #{self.model}, Year: #{self.year}"
+  end
 end
 
 # TODO: Implement class that inherits from Computer class
+
+# dell class
+class Dell < Computer
+
+end
+
+# lENOVO CLASS
+class Lenovo < Computer
+    def print_computer
+        puts "This is better than #3"
+  
+    end
+
+    def origin_print_computer
+       super
+       print_computer
+    end
+
+
+end
+
+
+
+xps = Dell.new("XPS 13","USA", 2022)
+xps.print_computer
+
+lenovo = Lenovo.new("X1 carbon","Italy", 2023)
+lenovo.print_computer
 
 # TODO: Use the super keyword to access attributes and methods from Computer
--- plant.rb
+++ plant.rb
@@ -1,1 +1,35 @@
-# TODO: Implement a class do illustrate how class variables and methods work.
+class Plant
+attr_accessor :name, :species
+
+# count class variable 
+@@count = 0
+
+  def initialize (name, species)
+    @name = name
+    @species = species
+    @@count += 1
+  end
+
+# count class method
+def self.count
+    @@count
+end
+
+end
+
+mango = Plant.new("mango", "maembe")
+puts "No of plants: #{Plant.count}"
+
+avocado = Plant.new("avocado", "parachichi")
+puts "No of plants: #{Plant.count}"
+
+apple = Plant.new("apple", "tufaha")
+puts "No of plants: #{Plant.count}"
+
+watermellon = Plant.new("watermellon", "tikitimaji")
+puts "No of plants: #{Plant.count}"
+
+pineapple = Plant.new("pineapple", "nanasi")
+puts "No of plants: #{Plant.count}"
+
+puts "Total: #{Plant.count}"
